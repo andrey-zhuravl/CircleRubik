dc33d94274beecd82d17560c74a2e7da7e22e0db
andrey-zhuravl <andrey_zhuravl@mail.ru>
2015-02-22 03:29:10 +0200

    ControledCommit

--- CircleRubik/CircleRubik.cpp
+++ CircleRubik/CircleRubik.cpp
@@ -18,7 +18,7 @@ void display(){
 
 	glColor3f(1.0, 0.0, 0.0);
 	glBegin(GL_QUADS); 
-	glVertex2f(0.5, 0.5);
+	glVertex2f(0.5, 0.6);
 	glVertex2f(0.5, -0.5); 
 	glVertex2f(-0.5, -0.5); 
 	glVertex2f(-0.5, 0.5); 
